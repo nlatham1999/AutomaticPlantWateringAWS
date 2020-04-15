a95736a4f39d5f7c484784ead16ff77f1c77d440
nlatham1999 <32083005+nlatham1999@users.noreply.github.com>
2020-04-15 15:11:53 -0700

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -3,15 +3,17 @@
 long timeToRun = 0;
 int garbage = 0;
 
+//setup function
 void setup() {
   Serial.begin(9600);
-  pinMode(A0, INPUT);
+  pinMode(A0, INPUT); //different pins
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
 }
 
+//main loop
 void loop() {
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) {  //if the serial is available then send the data 
     timeToRun = Serial.parseInt();
     garbage = Serial.parseInt();
     Serial.print(analogRead(A0));
